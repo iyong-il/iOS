634012380f36ed0acab8ab21c1a2044314bbd064
iyong-il <lyi1233313@gmail.com>
2023-03-15 22:10:24 +0900

    23.03.15 / 설정앱 만들어보기

---
+++ SwiftUI/sampleSetting/sampleSetting/sampleSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleSettingApp.swift
+//  sampleSetting
+//
+//  Created by 이용일(Rodi) on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct sampleSettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
